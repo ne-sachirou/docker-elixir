c5b8d1eb1d6b90b64ece4880e803ca7fb9a8f031
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-02-12 23:09:40 +0900

    OTP 21.3.8.13, OTP 22.2.6, Elixir 1.10.1

--- config/config.exs
+++ config/config.exs
@@ -3,15 +3,15 @@ use Mix.Config
 config :make,
   langs: %{
     clojerl: %{
-      versions: [%{version: "27cacfa9ce58c744db708a7f6d4f25ba703acdc9", major_version: "HEAD"}],
+      versions: [%{version: "417081ffe05451b7ce5e04f309a7786b69c377f1", major_version: "HEAD"}],
       latest_major_version: "HEAD",
       natural_name: "Clojerl",
       short_name: "clje"
     },
     erlang: %{
       versions: [
-        %{version: "21.3.8.12", major_version: "21", base_image: "alpine:3.9"},
-        %{version: "22.2.3", major_version: "22", base_image: "alpine:3.11"}
+        %{version: "21.3.8.13", major_version: "21", base_image: "alpine:3.9"},
+        %{version: "22.2.6", major_version: "22", base_image: "alpine:3.11"}
       ],
       latest_major_version: "22",
       natural_name: "Erlang/OTP",
@@ -20,7 +20,7 @@ config :make,
     elixir: %{
       versions: [
         %{version: "1.9.4", major_version: "1.9"},
-        %{version: "1.10.0", major_version: "1.10"}
+        %{version: "1.10.1", major_version: "1.10"}
       ],
       latest_major_version: "1.10",
       natural_name: "Elixir",
