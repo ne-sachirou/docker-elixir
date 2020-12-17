5990268001792daf17ad797acb00bf32ae8eb93c
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-12-17 12:51:17 +0900

    Erlang/OTP 22.3.4.13, Erlang/OTP 23.2

--- config/config.exs
+++ config/config.exs
@@ -12,8 +12,8 @@ config :make,
     },
     erlang: %{
       versions: [
-        %{version: "22.3.4.12", major_version: "22", base_image: "alpine:3.12"},
-        %{version: "23.1.4", major_version: "23", base_image: "alpine:3.12"}
+        %{version: "22.3.4.13", major_version: "22", base_image: "alpine:3.12"},
+        %{version: "23.2", major_version: "23", base_image: "alpine:3.12"}
       ],
       latest_major_version: "23",
       natural_name: "Erlang/OTP",
