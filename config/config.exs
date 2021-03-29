46262400f823b7d9322552bf6edee00e342b3946
ne-sachirou <ne-sachirou@hatena.ne.jp>
2021-03-29 12:46:03 +0900

    Erlang/OTP 23.3, Erlang/OTP 24.0-rc2, Elixir 1.11.4

--- config/config.exs
+++ config/config.exs
@@ -13,8 +13,8 @@ config :make,
     erlang: %{
       versions: [
         %{version: "22.3.4.16", major_version: "22", base_image: "alpine:3.12"},
-        %{version: "23.2.7", major_version: "23", base_image: "alpine:3.13"},
-        %{version: "24.0-rc1", major_version: "24", base_image: "alpine:3.13"}
+        %{version: "23.3", major_version: "23", base_image: "alpine:3.13"},
+        %{version: "24.0-rc2", major_version: "24", base_image: "alpine:3.13"}
       ],
       latest_major_version: "23",
       natural_name: "Erlang/OTP",
@@ -23,7 +23,7 @@ config :make,
     elixir: %{
       versions: [
         %{version: "1.10.4", major_version: "1.10"},
-        %{version: "1.11.3", major_version: "1.11"}
+        %{version: "1.11.4", major_version: "1.11"}
       ],
       latest_major_version: "1.11",
       natural_name: "Elixir",
