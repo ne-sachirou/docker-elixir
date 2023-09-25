64a72b9d7ee4563f7f12041f3c87822138ec05e5
ne-sachirou <ne-sachirou@hatena.ne.jp>
2023-09-26 02:49:35 +0900

    Erlang/OTP 26.1, Elixir 1.15.6

--- config/config.exs
+++ config/config.exs
@@ -5,18 +5,18 @@ config :make,
     erlang: %{
       versions: [
         %{version: "25.3.2.2", major_version: "25"},
-        %{version: "26.0.1", major_version: "26"},
+        %{version: "26.1", major_version: "26"},
       ],
-      latest_major_version: "25",
+      latest_major_version: "26",
       natural_name: "Erlang/OTP",
       short_name: "erl",
     },
     elixir: %{
       versions: [
         %{version: "1.14.5", major_version: "1.14"},
-        %{version: "1.15.0", major_version: "1.15"},
+        %{version: "1.15.6", major_version: "1.15"},
       ],
-      latest_major_version: "1.14",
+      latest_major_version: "1.15",
       natural_name: "Elixir",
       short_name: "ex",
     }
